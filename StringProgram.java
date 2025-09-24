b6b07884b283afd993663c1c298fe8d8af982fcc
Rexoschulz <schulzoliver111208@gmail.com>
2025-09-24 16:21:13 +0000

    string start

--- StringProgram.java
+++ StringProgram.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
 public class StringProgram {
   public static void main(String[] args) {
     // Complete a method to take in a String object from Scanner, remove all * symbols,  
     // and concatenate the four strings in reverse order. Print your new String.
-
+    Scanner scan = new Scanner(System.in);
+    System.out.println("")
     
   }
 }
