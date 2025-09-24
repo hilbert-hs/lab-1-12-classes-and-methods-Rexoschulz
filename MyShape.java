3c692f80f8790c40c9c2fa4a706502360d790f58
Rexoschulz <schulzoliver111208@gmail.com>
2025-09-24 15:54:56 +0000

    my shape done

--- MyShape.java
+++ MyShape.java
@@ -1,17 +1,32 @@
+import java.util.Scanner;
 public class MyShape {
   public static void main(String[] args) {
     // declare variables for length, width, and area
+    double length = 0.0;
+    double width = 0.0;
+    double area = 0.0;
 
     // use a Scanner object to get length and width values
-
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Give me a length");
+    length = scan.nextDouble();
+    System.out.println("Give me a width");
+    width = scan.nextDouble();
     // call your methods to test your program
-
+    area = calculateArea(length, width);
+    
+    System.out.println("The peri")
     
   }
 
   // write your perimeter method here
+  public static double calculatePeri(double _len, double _wid) {
+    return (_len * 2) + (_wid * 2);
+  };
 
 
   // write your area method here
-  
+  public static double calculateArea(double _len, double _wid) {
+    return _len * _wid;
+  };
 }
